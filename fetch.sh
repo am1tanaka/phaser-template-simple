53d0d26c6071bbb4b11296e54a62fac40db00a24
Yu Tanaka <githubam1@am1.jp>
2017-05-11 14:14:53 +0900

    ひな型のフォルダーとファイルを追加

---
+++ fetch.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+wget -O ./_site/js/phaser.min.js https://raw.githubusercontent.com/photonstorm/phaser-ce/master/build/phaser.min.js
+
+wget -O ./_site/assets/images/baddie.png https://github.com/photonstorm/phaser-ce/raw/master/resources/tutorials/02%20Making%20your%20first%20game/assets/baddie.png
